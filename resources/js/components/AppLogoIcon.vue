5818c1947b7f3cd5ffafd643b422585b4ea88f59
rok <post@robert-kummer.de>
2025-12-14 12:54:05 +0100

    chore: update favicon and associated SVG assets with new gradient design and adjustments

--- resources/js/components/AppLogoIcon.vue
+++ resources/js/components/AppLogoIcon.vue
@@ -15,26 +15,21 @@ defineProps<Props>();
 <template>
     <svg
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="215 213 83 133"
+        viewBox="0 0 144 144"
         :class="className"
         v-bind="$attrs"
     >
         <defs>
-            <linearGradient id="ghostGradient" x1="0" y1="0" x2="1" y2="1">
-                <stop offset="0%" stop-color="#6EE7B7" />
-                <stop offset="100%" stop-color="#3B82F6" />
+            <linearGradient id="a" x1="0" x2="1" y1="0" y2="1">
+                <stop offset="0" stop-color="#6EE7B7"/>
+                <stop offset="1" stop-color="#3B82F6"/>
             </linearGradient>
         </defs>
 
-        <g transform="translate(146, 146) scale(0.43)">
-            <path
-                d="M 160, 250
-                 A 96, 96 0 1, 1 352, 250  L 352, 420                Q 352, 490 256, 440       Q 160, 390 160, 300       Z"
-                fill="url(#ghostGradient)"
-            />
-
-            <ellipse cx="215" cy="260" rx="18" ry="28" fill="currentColor" />
-            <ellipse cx="297" cy="260" rx="18" ry="28" fill="currentColor" />
+        <g transform="matrix(.43 0 0 .43 -38.1 -60.17)">
+            <path fill="url(#a)" d="M160 250a96 96 0 1 1 192 0v170q0 70-96 20t-96-140Z"/>
+            <ellipse cx="215" cy="260" fill="currentColor" rx="18" ry="28"/>
+            <ellipse cx="297" cy="260" fill="currentColor" rx="18" ry="28"/>
         </g>
     </svg>
 </template>
