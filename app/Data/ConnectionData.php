9ac9636b9076fa2b0546829cae532cb9cca9eed8
rok <post@robert-kummer.de>
2025-12-15 18:02:53 +0100

    chore: initialize internalConnectionName in ConnectionData for reuse consistency

--- app/Data/ConnectionData.php
+++ app/Data/ConnectionData.php
@@ -8,13 +8,17 @@ use Illuminate\Support\Str;
 
 final readonly class ConnectionData
 {
+    private string $internalConnectionName;
+
     public function __construct(
         public string $name,
         public ConnectionDriverData $driver,
-    ) {}
+    ) {
+        $this->internalConnectionName = 'dyn_' . Str::random() . '_' . $name;
+    }
 
     public function connectionName(): string
     {
-        return 'dyn_' . Str::random() . '_' . $this->name;
+        return $this->internalConnectionName;
     }
 }
