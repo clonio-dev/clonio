acdca9c6918b403e892cda7a6553f201e232d445
rok <post@robert-kummer.de>
2026-01-11 14:06:22 +0100

    chore: update `StoreDatabaseConnectionRequest` to include validation rules and authorization logic for database connection creation

--- app/Http/Requests/StoreDatabaseConnectionRequest.php
+++ app/Http/Requests/StoreDatabaseConnectionRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use App\Enums\DatabaseConnectionTypes;
+use App\Models\DatabaseConnection;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreDatabaseConnectionRequest extends FormRequest
 {
@@ -11,7 +14,7 @@ class StoreDatabaseConnectionRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return $this->user()?->can('create', DatabaseConnection::class);
     }
 
     /**
@@ -22,7 +25,14 @@ class StoreDatabaseConnectionRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'type' => ['required', 'string', Rule::enum(DatabaseConnectionTypes::class)],
+            'host' => 'required|string|max:255',
+            'port' => 'required|integer|min:1|max:65535',
+            'database' => 'required|string|max:255',
+            'username' => 'required|string|max:255',
+            'password' => 'nullable|string|max:255',
+            'is_production_stage' => 'accepted',
         ];
     }
 }
