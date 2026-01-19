5a11e0a3445963565274f598e8686f8912b6723a
rok <post@robert-kummer.de>
2026-01-19 18:07:23 +0100

    chore: merge `CreateController` and `IndexController` into `DatabaseConnectionController` and update routes accordingly

--- app/Http/Controllers/DatabaseConnectionController.php
+++ app/Http/Controllers/DatabaseConnectionController.php
@@ -7,39 +7,47 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreDatabaseConnectionRequest;
 use App\Http\Requests\UpdateDatabaseConnectionRequest;
 use App\Models\DatabaseConnection;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class DatabaseConnectionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(): void
+    public function index(Request $request): Response
     {
-        //
+        $connections = DatabaseConnection::query()
+            ->forUser($request->user()->id)
+            ->paginate()
+            ->through(fn (DatabaseConnection $connection): array => [
+                'id' => $connection->id,
+                'name' => $connection->name,
+                'type' => $connection->type,
+                'host' => $connection->host,
+                'port' => $connection->port,
+                'database' => $connection->database,
+                'username' => $connection->username,
+                'is_production_stage' => $connection->is_production_stage,
+            ]);
+
+        return Inertia::render('connections/Index', [
+            'connections' => $connections,
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create(): void
+    public function store(StoreDatabaseConnectionRequest $request): RedirectResponse
     {
-        //
-    }
+        $connection = DatabaseConnection::query()->create([
+            'user_id' => $request->user()->id,
+            ...$request->validated(),
+            'password' => $request->string('password', ''),
+            'is_production_stage' => $request->boolean('is_production_stage'),
+        ]);
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreDatabaseConnectionRequest $request): void
-    {
-        //
-    }
+        // try to connect
+        $connection->update(['last_tested_at' => now()]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(DatabaseConnection $databaseConnection): void
-    {
-        //
+        return to_route('connections.index');
     }
 
     /**
--- routes/application/connections.php
+++ routes/application/connections.php
@@ -2,18 +2,17 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\DatabaseConnections\CreateController;
-use App\Http\Controllers\DatabaseConnections\IndexController;
+use App\Http\Controllers\DatabaseConnectionController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('connections')
     ->name('connections.')
     ->group(function (): void {
 
-        Route::get('/', IndexController::class)
+        Route::get('/', [DatabaseConnectionController::class, 'index'])
             ->name('index');
 
-        Route::post('/', CreateController::class)
+        Route::post('/', [DatabaseConnectionController::class, 'store'])
             ->name('store');
 
     });
