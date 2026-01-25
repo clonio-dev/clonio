d5bb22886a1b56a9e85c189211465098fc4f4e50
rok <post@robert-kummer.de>
2026-01-25 11:47:03 +0100

    chore: adjust formatting and indentation in `InfoComponent` for improved readability and consistency

--- resources/js/components/InfoComponent.vue
+++ resources/js/components/InfoComponent.vue
@@ -2,19 +2,30 @@
 import { LucideIcon } from 'lucide-vue-next';
 
 const props = defineProps<{
-    icon: LucideIcon,
-    title: string,
-    description: string,
+    icon: LucideIcon;
+    title: string;
+    description: string;
 }>();
 </script>
 
 <template>
-    <div class="p-6 rounded-xl bg-slate-100 dark:bg-[#1a2733] border border-dashed border-slate-300 dark:border-slate-700">
+    <div
+        class="rounded-xl border border-dashed border-slate-300 bg-slate-100 p-6 dark:border-slate-700 dark:bg-[#1a2733]"
+    >
         <div class="flex gap-4">
-            <component :is="props.icon" class="shrink-0 size-8 stroke-blue-600" />
+            <component
+                :is="props.icon"
+                class="size-8 shrink-0 stroke-blue-600"
+            />
             <div class="flex flex-col gap-1">
-                <h4 class="text-slate-900 dark:text-white font-bold text-lg" v-html="props.title"></h4>
-                <p class="text-slate-500 dark:text-[#92adc9] text-sm leading-relaxed" v-html="props.description"></p>
+                <h4
+                    class="text-lg font-bold text-slate-900 dark:text-white"
+                    v-html="props.title"
+                ></h4>
+                <p
+                    class="text-sm leading-relaxed text-slate-500 dark:text-[#92adc9]"
+                    v-html="props.description"
+                ></p>
             </div>
         </div>
     </div>
