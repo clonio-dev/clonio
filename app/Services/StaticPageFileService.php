2fa39e5ec4beec53f726f5afb759ab78bb35a09f
rok <post@robert-kummer.de>
2026-01-25 11:49:48 +0100

    chore: adjust formatting in `StaticPageFileService` for improved readability and consistency

--- app/Services/StaticPageFileService.php
+++ app/Services/StaticPageFileService.php
@@ -19,14 +19,14 @@ final class StaticPageFileService
 
     public function parse(string $content): StaticPage
     {
-        $parts = preg_split(self::PATTERN, PHP_EOL.mb_ltrim($content));
+        $parts = preg_split(self::PATTERN, PHP_EOL . mb_ltrim($content));
 
         if (count($parts) < 3) {
             return new StaticPage($content, new FrontMatter);
         }
 
         $data = Yaml::parse(mb_trim($parts[1]));
-        $body = implode(PHP_EOL.'---'.PHP_EOL, array_slice($parts, 2));
+        $body = implode(PHP_EOL . '---' . PHP_EOL, array_slice($parts, 2));
 
         return new StaticPage($body, new FrontMatter($data));
     }
