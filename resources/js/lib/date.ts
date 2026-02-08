52566f5c750d571aac5181fd92ee8d7a7b325f5a
rok <post@robert-kummer.de>
2026-02-08 13:11:28 +0100

    chore: improve readability of `convertMilliseconds` formatting and type definitions

--- resources/js/lib/date.ts
+++ resources/js/lib/date.ts
@@ -5,20 +5,41 @@
  * @return { d: number, h: number, m: number, s: number, total: { h: number, m: number, s: number }} - An object
  * containing days, hours, minutes, and seconds if with the total values.
  */
-export function convertMilliseconds(milliseconds: number): { d: number, h: number, m: number, s: number, total: { h: number, m: number, s: number }} {
-    const total_seconds = parseInt(Math.floor(milliseconds / 1000).toString(10), 10);
-    const total_minutes = parseInt(Math.floor(total_seconds / 60).toString(10), 10);
-    const total_hours = parseInt(Math.floor(total_minutes / 60).toString(10), 10);
+export function convertMilliseconds(milliseconds: number): {
+    d: number;
+    h: number;
+    m: number;
+    s: number;
+    total: { h: number; m: number; s: number };
+} {
+    const total_seconds = parseInt(
+        Math.floor(milliseconds / 1000).toString(10),
+        10,
+    );
+    const total_minutes = parseInt(
+        Math.floor(total_seconds / 60).toString(10),
+        10,
+    );
+    const total_hours = parseInt(
+        Math.floor(total_minutes / 60).toString(10),
+        10,
+    );
 
     const days = parseInt(Math.floor(total_hours / 24).toString(10), 10);
     const seconds = parseInt((total_seconds % 60).toString(10), 10);
     const minutes = parseInt((total_minutes % 60).toString(10), 10);
     const hours = parseInt((total_hours % 24).toString(10), 10);
 
-    return { d: days, h: hours, m: minutes, s: seconds, total: { h: total_hours, m: total_minutes, s: total_seconds} };
+    return {
+        d: days,
+        h: hours,
+        m: minutes,
+        s: seconds,
+        total: { h: total_hours, m: total_minutes, s: total_seconds },
+    };
 }
 
-export function convertDuration(start: Date, until?: Date|undefined): string {
+export function convertDuration(start: Date, until?: Date | undefined): string {
     const end: Date = until ? until : new Date();
 
     const diff = convertMilliseconds(end.getTime() - start.getTime());
