a23b1c2a67e1e5128ebacf89c78a10bf924e202c
rok <post@robert-kummer.de>
2026-02-12 17:33:15 +0100

    chore: add `UpdateDatabaseConnectionRequest` with validation rules and authorization logic

---
+++ app/Http/Requests/UpdateDatabaseConnectionRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use App\Enums\DatabaseConnectionTypes;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateDatabaseConnectionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()?->can('update', $this->route('connection'));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'type' => ['required', 'string', Rule::enum(DatabaseConnectionTypes::class)],
+            'host' => ['required', 'string', 'max:255'],
+            'port' => ['required', 'integer', 'min:1', 'max:65535'],
+            'database' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255'],
+            'password' => ['nullable', 'string', 'max:255'],
+            'is_production_stage' => ['sometimes', 'accepted'],
+        ];
+    }
+}
