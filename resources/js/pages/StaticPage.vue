8eebfdf687f7c7c0a23a9fdc5c0e634736f2a5b8
rok <post@robert-kummer.de>
2026-02-22 14:16:27 +0100

    chore: simplify navigation in `StaticPage.vue` by removing login/register links and updating dashboard logic

--- resources/js/pages/StaticPage.vue
+++ resources/js/pages/StaticPage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { dashboard, login, register } from '@/routes';
+import { dashboard } from '@/routes';
 import { Head, Link } from '@inertiajs/vue3';
 
 interface Props {
@@ -23,26 +23,18 @@ const props = defineProps<Props>();
         >
             <nav class="flex items-center justify-end gap-4">
                 <Link
+                    href="/"
+                    class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                >
+                    Clonio
+                </Link>
+                <Link
                     v-if="$page.props.auth.user"
                     :href="dashboard()"
                     class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                 >
                     Dashboard
                 </Link>
-                <template v-else>
-                    <Link
-                        :href="login()"
-                        class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
-                    >
-                        Log in
-                    </Link>
-                    <Link
-                        :href="register()"
-                        class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                    >
-                        Sign up
-                    </Link>
-                </template>
             </nav>
         </header>
         <div
